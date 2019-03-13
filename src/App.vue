418f220c1732d6faa45a302b080cf55267446379
MathildeBa <baquet.mathilde@gmail.com>
2019-03-13 12:36:02 +0100

    truc

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <zeroTalents></zeroTalents>
+    
+  </div>
+</template>
+
+<script>
+import zeroTalents from './components/zeroTalents.vue';
+
+export default {
+  name: 'app',
+  components: {
+    zeroTalents
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/zeroTalents.vue
@@ -0,0 +1,29 @@
+<template>
+<div id="app">
+  <h2 > {{title1}} </h2>
+  <h1>{{title2}}</h1>
+  <ul>
+    <li v-for="(elements, index) in liste" :key="index">{{ elements }}</li>
+  </ul>
+</div>
+</template>
+
+<script>
+  //zeroTalents.vue
+    export default {
+      name: 'app',
+      data(){
+        return {
+          title1 : '10 THINGS THAT REQUIRE',
+          title2 : 'ZERO TALENT',
+          liste : ["Being On Time","Making An Effort","Being High Energy","Having A Positive Attitude","Being Passionate","Using Good Body Language","Being Coachable","Doing A Little Extra","Being Prepared","Having A Strong Work Ethic"]
+        } 
+      }
+    }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
